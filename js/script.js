0a963b76430cc405f2b5e3a8f635a68d2bb0f304
hafidhdark-cmyk <hafidhdark@gmail.com>
2025-12-16 10:55:06 +0100

    add am

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*======================================= typing animation ============================= */
 var typed = new Typed(".typing", {
-    strings:["Web Designer","Web Developer","Graphic Designer","Freelancer"],
+    strings:["Web Designer","Web Developer","Software Engineer","Freelancer","Full-stack Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true      
